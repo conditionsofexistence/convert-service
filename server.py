20451773c7719a20a8bdc0af8f8d01ce02de2488
Davide Semenzin <davide@archive.org>
2016-11-25 18:29:19 -0800

    Service now works

--- server.py
+++ server.py
@@ -1,18 +1,52 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
 from converter.convert import convert
 from converter.convert import ingest
+import subprocess, uuid
 
 UPLOAD_FOLDER = '/tmp/'
+WORKSPACE_FOLDER = '/tmp/conversion'
 ALLOWED_EXTENSIONS = set(['csv'])
+
+if not os.path.exists(WORKSPACE_FOLDER):
+    os.makedirs(WORKSPACE_FOLDER)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['WORKSPACE_FOLDER'] = WORKSPACE_FOLDER
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def convert(filepath, request):
+    uid = uuid.uuid4().get_hex()
+    header = "REQUESTED BY", request.remote_addr
+
+    workspace = os.path.join(WORKSPACE_FOLDER, uid)
+    os.makedirs(workspace)
+    # copy filepath to worspace and invoke there
+    print workspace
+    out_path = os.path.join(workspace, 'output.xml')
+    out, err = subprocess.Popen(['python','converter/convert.py', 
+        '--input', filepath ,
+        '--output', out_path, 
+        '-p', 'none' ],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    out = out.splitlines()
+    err = [x.decode(encoding='ascii') for x in err.splitlines()]
+    print len(err), type(err)
+    print err
+    #log = proc.stderr.read()
+    print header
+    return out_path, err, uid
+
+@app.route('/download/<path:path>',  methods=['GET',])
+def send_js(path):
+    print path
+    return send_from_directory(WORKSPACE_FOLDER, path)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -25,11 +59,17 @@ def index():
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             print filepath
             data, size, input_file = ingest(filepath, 'none')
-            s = convert(data, size)
-            return s
-            #return redirect(url_for('index'))
+            converted_file, log, uid= convert(filepath, request)
 
-    # GET
+            ret = """
+                <!doctype html>
+                <head>
+                <title>CSV to TEI4BPS converter | Conversion complete!</title>
+                <h1>Conversion log</h1>
+                <a href = "/download/{2}/output.xml"> File: {1}</a>
+                <p style='font-family:"Andale Mono", "Monotype.com", monospace;'>{0}</p>""".format("<br/>".join([line for line in log]), converted_file, uid)
+            return ret
+
     return """
     <!doctype html>
     <title>CSV to TEI4BPS converter</title>
